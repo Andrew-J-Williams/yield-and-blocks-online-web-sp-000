c2bf56032c206ce476327a99697cca8a70fe8d97
Andrew Williams <1williamsaj@gmail.com>
2019-09-01 21:38:04 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,9 +9,9 @@ def hello_t(array)
 end
 
 # call your method here!
+names_array = ["Tim", "Tom", "Jim"]
 
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(names_array) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
